80043176f7881c2309f59ff0279ff865a3776434
DanielOng2603635 <2603635@sit.singaporetech.edu.sg>
2026-09-16 06:36:39 +0800

    Reject negative numbers

--- auditor.py
+++ auditor.py
@@ -1,10 +1,13 @@
 inventory = 0
 stock = 0
+
 while stock != "quit":
     stock = input("Enter a stock quantity:")
     if stock == "quit":
         break
-    elif stock.isdigit() == False:
+    elif stock.lstrip("-").isdigit() == False:
         print("Please enter a valid integer.")
     else:
         stock = int(stock)
+        if stock < 0:
+            print("Please enter a non-negative number.")
