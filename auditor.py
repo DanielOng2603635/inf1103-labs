f3a64509b0885f74121de09a82cb8b78e604d087
DanielOng2603635 <2603635@sit.singaporetech.edu.sg>
2026-09-16 06:27:39 +0800

    Handle valid input

--- auditor.py
+++ auditor.py
@@ -2,4 +2,9 @@ inventory = 0
 stock = 0
 while stock != "quit":
     stock = input("Enter a stock quantity:")
-    stock = int(stock)
+    if stock == "quit":
+        break
+    elif stock.isdigit() == False:
+        print("Please enter a valid integer.")
+    else:
+        stock = int(stock)
